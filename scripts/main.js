b5d3818f42c188617039be3e6f3e1920d00de810
auguchenco <auguchencomorales@gmail.com>
2024-08-28 22:05:39 -0300

    Se agrego la funcionalidad de ordenar la lista y se solucionaron problemas con la imagen de fondo, ademas de ajustes en los textos que se imprimian en pantalla, puntualmente en las fechas. Se corrigieron pequeños errores de funcionalidades en casos donde se utilizaron datos 'anormales' para hacer pruebas

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 const TODO_KEY = "taskList";
 
+
+const sortConfig = {
+    text: undefined,
+    date: undefined,
+    state: "all",
+    sortBy: "add",
+    invert: false,
+};
+// {
+//     text: string
+//     date: "yyy-mm-dd"
+//     state: pending completed all
+//     sortBy: add title date
+//     invert: bool
+// };
+
 // --- --- --- AUX FUNCTIONS --- --- ---
 
 // --- --- --- LOCAL STORAGE AUX FUNCTIONS
@@ -113,6 +129,7 @@ function getPopUpFormHTML(formTitle, form) {
 function getPopUpFormTaskHTML(formType) {
     // Creamos el elemento form
     const formHTML = document.createElement("form");
+    formHTML.id = "formTask";
     formHTML.classList.add("form");
     formHTML.innerHTML = `
                         <fieldset class="fieldset">
@@ -145,7 +162,6 @@ function getPopUpFormTaskHTML(formType) {
                         </div>
                             `;
 
-    formHTML.id = "formTask";
     let formTitle = '';
     switch (formType) {
         case "new-task":
@@ -184,8 +200,80 @@ function getPopUpFormTaskHTML(formType) {
 
 // Esta funcion crea un PopUpSortFormHTML
 // Sirve para ordenar la lista de tareas
-function getPopUpFormSortHTML() {
+function getPopUpFormSortHTML(sortConfig) {
+    // Creamos el elemento form
+    const formHTML = document.createElement("form");
+    formHTML.classList.add("form");
+    const text = (sortConfig.text) ? (sortConfig.text) : '';
+    formHTML.innerHTML = `
+                        <fieldset class="fieldset">
+                            <legend class="legend">Filter</legend>
 
+                            <div class="input__container">
+                                <label for="formTaskText" class="label">Text:</label>
+                                <input type="text" placeholder="Do something..." maxlength="30" name="text" id="formTaskText" class="input" value=${text}>
+                            </div>
+
+                            <div class="input__container">
+                                <label for="formTaskDate" class="label">Date:</label>
+                                <input type="date" name="date" id="formTaskDate" class="input" value=${sortConfig.date}>
+                            </div>
+
+                            <div class="input__container">
+                                <label for="formTaskState" class="label">State:</label>
+                                <select name="state" id="formTaskState" class="input" required>
+                                    <option value=${sortConfig.state}></option>
+                                    <option value="all">All</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="completed">Completed</option>
+                                </select>
+                            </div>
+
+                        </fieldset>
+
+                        <fieldset  class="fieldset">
+                            <legend class="legend">Sort</legend>
+
+                            <div class="input__container">
+                                <label for="formTaskSortBy" class="label">Sort by:</label>
+                                <select name="sortBy" id="formTaskSortBy" class="input" value=${sortConfig.sortBy} required>
+                                    <option value=${sortConfig.sortBy}></option>
+                                    <option value="add">Add</option>
+                                    <option value="title">Title</option>
+                                    <option value="date">Date</option>
+                                </select>
+                            </div>
+
+                            <div class="input__container">
+                                <label for="formTaskAscOrd" class="label">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-sort-descending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l9 0" /><path d="M4 12l7 0" /><path d="M4 18l7 0" /><path d="M15 15l3 3l3 -3" /><path d="M18 6l0 12" /></svg>
+                                </label>
+                                <input type="radio" name="invert" value="false" id="formTaskAscOrd" class="input">
+
+                                <label for="formTaskDescOrd" class="label">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l7 0" /><path d="M4 12l7 0" /><path d="M4 18l9 0" /><path d="M15 9l3 -3l3 3" /><path d="M18 6l0 12" /></svg>
+                                </label>
+                                <input type="radio" name="invert" value="true" id="formTaskDescOrd" class="input">
+                            </div>
+                        </fieldset>
+
+
+
+                        <div class="buttons__container">
+                        <button type="button" id="resetSortConfig" class="button">
+                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-restore"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3.06 13a9 9 0 1 0 .49 -4.087" /><path d="M3 4.001v5h5" /><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>    
+                        
+                        </button>
+                        <button type="submit" id="sortTaskBtn" class="button">
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l7 0" /><path d="M4 12l7 0" /><path d="M4 18l9 0" /><path d="M15 9l3 -3l3 3" /><path d="M18 6l0 12" /></svg>
+
+                        </button>
+                        </div>
+                        `;
+    
+    formHTML.addEventListener("submit", sortTaskList);
+    formHTML.querySelector("#resetSortConfig").addEventListener("click", resetSortConfig);
+    return getPopUpFormHTML("Sort Task List", formHTML);
 }
 
 
@@ -265,10 +353,50 @@ function deleteTask(taskId) {
     renderTaskListHTML(taskList);
 }
 
-function sortTaskList() {
-    const taskList = getTaskList();
-    const form = getPopUpFormSortHTML();
+function resetSortConfig() {
+    sortConfig.text = undefined;
+    sortConfig.text = undefined;
+    sortConfig.date = undefined;
+    sortConfig.state = "all";
+    sortConfig.sortBy = "add";
+    sortConfig.invert = false;
+    document.querySelector(".form").reset();
+    document.querySelector("#popUp").remove();
+    getPopUpFormSortHTML(sortConfig);
+};
 
+function sortTaskListAux(taskList, sortConfig) {
+    if (sortConfig.text) { taskList = taskList.filter( (task) => `${task.title} ${task.description}`.toLowerCase().includes(`${sortConfig.text}`.toLowerCase()) ); };
+    if (sortConfig.date) { taskList = taskList.filter( (task) => task.date == sortConfig.date ); };
+    if (sortConfig.state === "pending") { taskList = taskList.filter ( (task) => task.isCheck === false ); }
+    else if (sortConfig.state === "completed") { taskList = taskList.filter ( (task) => task.isCheck === true ); };
+    if (sortConfig.sortBy === "title") { taskList.sort((a,b) => a.title - b.title); }
+    else if (sortConfig.sortBy === "date") { taskList.sort((a,b) => a.date - b.date); };
+    if (sortConfig.invert == "true") { taskList.reverse(); };
+    return taskList;
+};
+
+
+
+function sortTaskList(event) {
+    event.preventDefault();
+
+    const form = event.target; // = document.querySelector("#formTask")
+    const formData = new FormData(form);
+
+    // Creamos el objeto newTask y seteamos los atributos capturados del formulario
+
+    formData.forEach((value, key) => {
+        sortConfig[key] = value;
+    });
+
+    // Si la tarea existia previamente la actualizamos sino la agregamos
+    // sortedTaskList = sortTaskListAux(getTaskList(), sortConfig);
+
+    form.reset();
+    document.querySelector("#popUp").remove();
+    renderTaskListHTML(getTaskList());
+    
 }
 
 
@@ -282,6 +410,7 @@ function renderTask(task) {
     taskHTML.id = task.id;
     taskHTML.classList.add("task");
     taskHTML.ariaLabel = `Tarea: ${task.title}`
+    const date = task.date.split("-");
     taskHTML.innerHTML = `
                     <div class="task__header">
                         <input type="checkbox" aria-label="Checkbox: marca tu tarea ${task.title} como completada" class="input">
@@ -297,7 +426,7 @@ function renderTask(task) {
                         <p aria-label="Descripción de la tarea ${task.title}: ${task.description}">${task.description}</p>
                     </div>
                     <div class="task__footer">
-                        <span aria-label="Fecha de la tarea: ${task.title}">${task.date}</span>
+                        <span aria-label="Fecha de la tarea: ${task.title}">${date[2]} / ${date[1]} / ${date[0]}</span>
                     </div>
     `;
 
@@ -322,6 +451,9 @@ function renderTask(task) {
 
 // Renderiza (agrega al DOM) la lista de objetos "Task" almacenados en el local storage.
 function renderTaskListHTML(taskList) {
+    // Ordenar la lista
+    taskList = sortTaskListAux(taskList, sortConfig);
+    
     // Seleccionamos la lista y la vaciamos
     const taskListHTML = document.querySelector("#taskList");
     taskListHTML.innerHTML = '';
@@ -344,7 +476,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     // Agregamos el evento de ordenar lista de tareas al boton #sortTaskListBtn
     document.querySelector("#sortTaskListBtn").addEventListener("click", () => {
-        sortTaskList();
+        getPopUpFormSortHTML(sortConfig);
     });
     
     // Cargamos las etiquetas de accesibilidad
